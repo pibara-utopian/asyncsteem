c2f616e6d30d533522ca80ef20e1b1abe80445d7
Rob J Meijer <pibara@gmail.com>
2018-01-29 22:42:08 +0100

    added speed info

--- asyncsteem_benchmark.py
+++ asyncsteem_benchmark.py
@@ -6,13 +6,17 @@ import time
 class TestBot:
     def __init__(self):
         self.blocks = 0
+        self.start = time.time()
         self.last = time.time()
     def block(self,tm,event,cont):
         self.blocks = self.blocks + 1
-        if self.blocks % 1000 == 0:
+        if self.blocks % 100 == 0:
             now = time.time()
             duration = now - self.last
-            print "* 1000 blocks processed in",duration,"seconds."
+            total_duration = now - self.start
+            speed = int(100000.0/duration)*1.0/1000
+            avspeed = int(self.blocks*1000/total_duration)*1.0/1000
+            print "* 100 blocks processed in",duration,"seconds. Speed",speed,"blocks per second. Avg:",avspeed
             self.last = now
 
 print "Constructing ActiveBlockChain"
