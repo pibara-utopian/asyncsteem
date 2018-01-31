0d54e9ce79257f4695e5220b865a20937c0d27b1
Rob J Meijer <pibara@gmail.com>
2018-02-01 00:11:10 +0100

    added more hints about issues needing a fix

--- watching_the_watchers.py
+++ watching_the_watchers.py
@@ -13,28 +13,18 @@ class WatchingTheWatchersBot:
         self.last = time.time()
         self.queue = AsyncQueue(reactor)
     def _comment(self,obj):
-        print "OBJ:", obj
+        print "OBJ:", obj #FIXME: process the comment
     def vote(self,tm,event,cont):
         if event["weight"] < 0:
             print colored(str(tm),"cyan"),colored("@" + event["voter"],"yellow"),"=> "+colored("@"+event["author"],"green") + "/" + event["permlink"]
             self.queue.get_comment(event["author"],event["permlink"])(self._comment)
-    def hour(self,tm,event,cont):
-        self.hourcount = self.hourcount + 1
-        now = time.time()
-        total_duration = str(timedelta(seconds=now-self.start))
+    def day(self,tm,event,cont):
         print colored("* HOUR mark: Processed "+str(self.hourcount)+ " blockchain hours in "+ total_duration,"green")
-        if self.hourcount == 24: 
-            print "Ending eventloop"
-            reactor.stop()
+        reactor.stop() #FIXME, we shouldn't stop the reacor just yet, but ActiveBlockChain should sop emitting events and
+                       #       after teh queue is completely empty and all pending queries answered, the reactor should stop
 
-print "Constructing ActiveBlockChain"
 bc = ActiveBlockChain(reactor,rewind_days=1,parallel = 8)
-print "Constructing bot"
 tb = WatchingTheWatchersBot()
-print "Regestering bot"
 bc.register_bot(tb,"watchingthewatchers")
-print "Starting bot"
 bc.start()
-print "Starting main event loop"
 reactor.run()
-print "Done"
