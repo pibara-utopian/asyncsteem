14f205093a1ac2d102a07a33aea33d18d6c9555c
Rob J Meijer <pibara@gmail.com>
2018-02-06 00:11:57 +0100

    a bit of comment in main

--- asyncsteem/jsonrpc_api_v2.py
+++ asyncsteem/jsonrpc_api_v2.py
@@ -231,36 +231,66 @@ class RpcClient(object):
 
 if __name__ == "__main__":
     from twisted.internet import reactor
+    #When processing a block we call this function for each downvote/flag
     def process_vote(vote_event,clnt):
+        #Create a new JSON-RPC entry on the queue to fetch post info, including detailed vote info
         opp = clnt.get_content(vote_event["author"],vote_event["permlink"])
+        #This one is for processing the results from get_content
         def process_content(event, client):
+            #We geep track of votes given and the total rshares this resulted in.
             start_rshares = 0.0
+            #Itterate over all votes to count rshares and to find the downvote we are interested in.
             for vote in  event["active_votes"]:
+                #Look if it is our downvote.
                 if vote["voter"] == vote_event["voter"] and vote["rshares"] < 0:
+                    #Diferentiate between attenuating downvotes and reputation eating flags.
                     if start_rshares + float(vote["rshares"]) < 0:
-                        print vote["time"],"FLAG",vote["voter"],"=>",vote_event["author"],vote["rshares"],"(",start_rshares , "->", start_rshares + float(vote["rshares"]) , ")" 
+                        print vote["time"],\
+                              "FLAG",\
+                              vote["voter"],"=>",vote_event["author"],\
+                              vote["rshares"]," rshares (",\
+                              start_rshares , "->", start_rshares + float(vote["rshares"]) , ")" 
                     else:
-                        print vote["time"],"DV",vote["voter"],"=>",vote_event["author"],vote["rshares"],"(",start_rshares , "->" , start_rshares + float(vote["rshares"]) , ")"
+                        print vote["time"],\
+                              "DOWNVOTE",\
+                              vote["voter"],"=>",vote_event["author"],\
+                              vote["rshares"],"(",\
+                              start_rshares , "->" , start_rshares + float(vote["rshares"]) , ")"
+                #Update the total rshares recorded before our downvote
                 start_rshares = start_rshares + vote["rshares"]
+        #Set the above closure as callback.
         opp.on_result(process_content)
+    #This is a bit fiddly at this low level,  start nextblock a bit higer than where we start out
     nextblock = 19000008
+    #Create our JSON-RPC RpcClient
     rpcclient = RpcClient(reactor)
+    #Function for fetching a block and its operations.
     def get_block(blk):
         """Request a single block asynchonously."""
+        #Create a new JSON-RPC entry on the queue to fetch a block.
         opp = rpcclient.get_block(blk)
+        #This one is for processing the results from get_block
         def process_block(event, client):
             """Process the result from block getting request."""
             global nextblock
             if event != None:
+                #Itterate over all operations in the block.
                 for t in event["transactions"]:
                     for o in t["operations"]:
+                        #We are only interested in downvotes
                         if o[0] == "vote" and o[1]["weight"] < 0:
+                            #Call process_vote for each downvote
                             process_vote(o[1],client)
-            print "get_block",nextblock,len(client.queue),client.active_call_count
+            #If we done with one block, try to fetch one more, always try to keep upto 8 active block
+            #fetching network clients alive.
             get_block(nextblock)
             nextblock = nextblock + 1
+        #Bind the above closure to the result of get_block
         opp.on_result(process_block)
+    #Kickstart the process by kicking off eigth block fetching operations.
     for block in range(19000000, 19000008):
         get_block(block)
+    #By invoking the rpcclient, we will process queue entries upto the max number of paralel HTTPS requests.
     rpcclient()
+    #Start the main twisted event loop.
     reactor.run()
