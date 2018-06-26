942348a9bff9f46cd9cc48f8ca0fb95596e0929b
Rob J Meijer <pibara@gmail.com>
2018-06-26 23:46:06 +0200

    as far as we will get with visjs without adding websockets

--- sample_code/flag-stream/flag_stream.py
+++ sample_code/flag-stream/flag_stream.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 import json
 import io
-from twisted.internet import reactor
+from twisted.internet import reactor, endpoints
 from twisted.logger import Logger, textFileLogObserver
 from twisted.web import server,resource
+from twisted.web.static import File
 from asyncsteem import ActiveBlockChain
 from os.path import join, dirname, realpath
 
@@ -19,27 +20,64 @@ class FlagHub:
     def get_log(self):
         return self.log
 
-class FlagWebServer(resource.Resource):
+
+
+class SnapShot(resource.Resource):
     isLeaf = True
     def __init__(self,hub):
         self.hub = hub
     def render_GET(self, request):
-        head = '<html><head><meta http-equiv="refresh" content="15"></head><body><ul>\n' 
-        body = ""
-        m = 0
+        vertexes = set()
+        edges = set()
         count = 0
         log = self.hub.get_log()
         for minute in reversed(log):
             for cell in reversed(minute):
                 count += 1
-                if count < 30:
-                    line = "<li>" + cell[0] + " -> " + cell[1] + " (" + str(m) +")</li>\n"
-                    body = body + line
-            m += 1
-        if count == 0:
-            body = "<li>WAIT (" + str(len(log)) + ")</li>\n"
-        foot = "\n</ul></body></html>"
-        return bytes(head + body + foot)
+                if count < 40:
+                    vertex1 = cell[0]
+                    vertex2 = cell[1]
+                    edge = vertex1 + "->" + vertex2
+                    vertexes.add(vertex1)
+                    vertexes.add(vertex2)
+                    edges.add(edge);
+        rval = {}
+        rval["nodes"] = []
+        name2num = {}
+        v_id = 0
+        for vertex in vertexes:
+            v_id += 1
+            name2num[vertex] = v_id
+            nod = {}
+            nod["id"] = v_id
+            nod["label"] = vertex
+            rval["nodes"].append(nod)
+        rval["edges"] = []
+        for edge in edges:
+            [node1,node2] = edge.split("->")
+            edg = {}
+            edg["from"] = name2num[node1]
+            edg["to"] = name2num[node2]
+            edg["arrows"] = "to"
+            rval["edges"].append(edg)
+        return json.dumps(rval)
+
+class FlagWebServer(resource.Resource):
+    def __init__(self,hub):
+        self.hub = hub
+        self.children = ["/","/snapshot"]
+    def getChild(self, name, request):
+        if request.uri == '/snapshot':
+            return SnapShot(self.hub)
+        else:
+            if request.uri == '/vis.js':
+                return File("visjs/dist/vis.js")
+            else:
+                if request.uri == '/':
+                    return File("index.html")
+                else:
+                    return resource.NoResource()
+
 
 class FlagStream:
     def __init__(self,hub):
@@ -54,13 +92,15 @@ class FlagStream:
             self.hub.tick()
 
 mypath = dirname(realpath(__file__))
-obs = textFileLogObserver(io.open(join(mypath,"flag_stream.log"), "a"))
-log = Logger(observer=obs,namespace="asyncsteem")
-bc = ActiveBlockChain(reactor,log=log,nodelist="stage")
+observer = textFileLogObserver(io.open(join(mypath,"flag_stream.log"), "a"))
+logger = Logger(observer=observer,namespace="asyncsteem")
+blockchain = ActiveBlockChain(reactor,log=logger,nodelist="stage")
 hub = FlagHub()
-tb = FlagStream(hub)
-bc.register_bot(tb,"flag_stream")
-ws = FlagWebServer(hub)
-s = server.Site(ws)
-reactor.listenTCP(4080,s)
+steembot = FlagStream(hub)
+blockchain.register_bot(steembot,"flag_stream")
+#
+root = FlagWebServer(hub)
+factory = server.Site(root)
+endpoint = endpoints.TCP4ServerEndpoint(reactor, 4080)
+endpoint.listen(factory)
 reactor.run()
